fcf5cf50f53661a469199ec03ae04a58db1c624c
Dworv <dwarvyt@gmail.com>
2022-02-03 17:21:16 -0800

    added bot framework

--- downfall/bot.py
+++ downfall/bot.py
@@ -1,3 +1,13 @@
 
 # the bot file :D
 # never give up!
+
+import interactions
+from secret import bot_secret
+
+bot: interactions.Client = interactions.Client(token=bot_secret)
+
+
+
+print("Bot started")
+bot.start()
