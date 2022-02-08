f9342ae6ab834fa25788dba791977f542e9bb9b7
Dworv YT <dwarvyt@gmail.com>
2022-02-08 12:38:18 -0800

    changed scope to access from config file

--- downfall/cogs/utils.py
+++ downfall/cogs/utils.py
@@ -6,7 +6,7 @@ class Utils(interactions.Extension):
     def __init__(self, client: interactions.Client):
         self.client: interactions.Client = client
     
-    @interactions.extension_command(name="ping", description="Pings the bot", scope=[896513855763652669])
+    @interactions.extension_command(name="ping", description="Pings the bot", scope=[config.Guild.ID])
     async def ping(self, ctx: interactions.CommandContext):
         await ctx.send("Pong!")
 
