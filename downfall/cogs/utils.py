8175f6410ff0b34d9fb394dc56809401a719b417
Dworv <dwarvyt@gmail.com>
2022-02-09 08:09:34 -0800

    added channel checks

--- downfall/cogs/utils.py
+++ downfall/cogs/utils.py
@@ -3,28 +3,32 @@ import random
 import interactions 
 
 import config
-from tools.embed import create_info_embed, create_error_embed
+from tools import embed, check
+
 
 
 class Utils(interactions.Extension):
 
     def __init__(self, client: interactions.Client):
         self.client: interactions.Client = client
-    
+
     @interactions.extension_command(
         name="ping", 
         description="Pings the bot", 
         scope=[config.Guild.ID]
     )
     async def ping(self, ctx: interactions.CommandContext):
-        await ctx.send(embeds=create_info_embed(description="Pong!"))
+        await ctx.send(embeds=embed.create_info_embed(description="Pong!"))
 
     @interactions.extension_command(
         name="coin-flip", 
         description="Flips a coin", 
         scope=[config.Guild.ID])
     async def coin_flip(self, ctx: interactions.CommandContext):
-        await ctx.send(embeds=create_info_embed(description=f"You flipped a coin and got **{random.choice(['heads', 'tails'])}**!"))
+        if illegal := check.illegal_commmand_channel(ctx.channel_id):
+            await ctx.send(embeds=illegal, ephemeral=True)
+            return
+        await ctx.send(embeds=embed.create_info_embed(description=f"You flipped a coin and got **{random.choice(['heads', 'tails'])}**!"))
     
     dice_sides = ['4', '6', '8', '10', '12', '20']
     @interactions.extension_command(
@@ -42,7 +46,10 @@ class Utils(interactions.Extension):
         ]
     )
     async def roll_dice(self, ctx: interactions.CommandContext, sides: str):
-        await ctx.send(embeds=create_info_embed(description=f"You rolled a **{random.randint(1, int(sides))}** from a **{sides}** sided dice!"))
+        if illegal := check.illegal_commmand_channel(ctx.channel_id):
+            await ctx.send(embeds=illegal, ephemeral=True)
+            return
+        await ctx.send(embeds=embed.create_info_embed(description=f"You rolled a **{random.randint(1, int(sides))}** from a **{sides}** sided dice!"))
 
 def setup(client: interactions.Client):
     Utils(client)
