5b25d17f30f6b3110afd7b648cff109d53335d19
Dworv <dwarvyt@gmail.com>
2022-02-16 21:32:25 -0800

    fixed my ideocy thank god man

--- downfall/tools/database.py
+++ downfall/tools/database.py
@@ -9,9 +9,6 @@ class DBFail:
     def __init__(self, message):
         self.message = message
         self.embeds = create_error_embed(self.message)
-    
-    def __bool__(self):
-        return False
 
 # user stuff
 
@@ -21,35 +18,35 @@ class UserTrait:
     YOUTUBE = 'youtube'
     BIO = 'bio'
 
-async def new_user(user_id, editor_level, youtube, bio):
+def new_user(user_id, editor_level, youtube, bio):
     try:
         c.execute("INSERT INTO users VALUES (?, ?, ?, ?)", (user_id, editor_level, youtube, bio))
         con.commit()
     except:
         return DBFail("Failed to create user.")
 
-async def modify_user(user_id, trait, entry):
+def modify_user(user_id, trait, entry):
     try:
         c.execute("UPDATE users SET ? = ? WHERE user_id = ?", (trait, entry, user_id))  
         con.commit()
     except:
         return DBFail("Failed to modify user.")
 
-async def get_user(user_id):
+def get_user(user_id):
     try:
         c.execute("SELECT * FROM users WHERE user_id = ?", (user_id))
         return c.fetchone()
     except:
         return DBFail("Failed to get user.")
 
-async def all_users():
+def all_users():
     try:
         c.execute("SELECT * FROM users")
         return c.fetchall()
     except:
         return DBFail("Failed to get all users.")
 
-async def delete_user(user_id):
+def delete_user(user_id):
     try:
         c.execute("DELETE FROM users WHERE user_id = ?", (user_id))
         con.commit()
@@ -65,39 +62,39 @@ class ApplicationTrait:
     URL = 'url'
     PRERECS = 'prerecs'
 
-async def new_application(user_id, url, prerecs):
+def new_application(user_id, url, prerecs):
     try:
-        c.execute("INSERT INTO applications (status, user_id, url, prerecs) VALUES (?, ?, ?, ?)", (-1, user_id, url, prerecs))
+        c.execute("INSERT INTO applications (status, user_id, url, prerecs) VALUES (?, ?, ?, ?)", (-1, int(user_id), str(url), int(prerecs)))
         con.commit()
     except:
         return DBFail("Failed to create application.")
 
-async def modify_application(application_id, trait, entry):
+def modify_application(application_id, trait, entry):
     try:
         c.execute("UPDATE applications SET ? = ? WHERE application_id = ?", (trait, entry, application_id))  
         con.commit()
     except:
         return DBFail("Failed to modify application.")
 
-async def get_application(application_id):
+def get_application(application_id):
     try:
         c.execute("SELECT * FROM applications WHERE application_id = ?", (application_id))
         return c.fetchone()
     except:
         return DBFail("Failed to get application.")
 
-async def all_applications():
+def all_applications():
     try:
         c.execute("SELECT * FROM applications")
         return c.fetchall()
     except:
         return DBFail("Failed to get all applications.")
 
-async def delete_application(application_id):
+def delete_application(application_id):
     try:
         c.execute("DELETE FROM applications WHERE application_id = ?", (application_id))
         con.commit()
     except:
         return DBFail("Failed to delete application.")
 
-
+new_application(1234234, 'asdfdf', True)
