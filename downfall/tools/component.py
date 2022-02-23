54714c15e1e43b2403c886f24ea1b87045a611f1
Dworv YT <dwarvyt@gmail.com>
2022-02-23 12:44:56 -0800

    finished custom_id decode/encode

--- downfall/tools/component.py
+++ downfall/tools/component.py
@@ -1,11 +1,19 @@
 
-def custom_to_dict(custom_id) -> dict:
+def decode_custom_id(custom_id: int) -> tuple:
     split: list = custom_id.split('?')
+
     entry_count = len(split) - 1
     if not entry_count:
         return {'base': custom_id}
-    entries = {'base': split.pop(0)}
+    base = split.pop(0)
+    entries = {}
     for entry in split:
         name, value = entry.split('=')
         entries[name] = value
-    return entries
+    return base, entries
+
+def encode_custom_id(base, entries: dict) -> str:
+    custom = base
+    entry_list = [f'?{name}={value}' for name, value in entries.items()]
+    for entry in entry_list: custom += entry
+    return custom
