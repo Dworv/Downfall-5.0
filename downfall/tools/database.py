873a8ec33ba3a743f5308177b71a9c3704f4fbe2
Dworv <dwarvyt@gmail.com>
2022-02-23 08:04:41 -0800

    created user if not found and fixed tuple commas

--- downfall/tools/database.py
+++ downfall/tools/database.py
@@ -2,7 +2,7 @@
 import sqlite3
 from tools.embed import create_error_embed
 
-con = sqlite3.connect('C:/Dworv Stuff/Coding/lapsus/downfall/downfall.db', timeout = 2)
+con = sqlite3.connect('C:/Dworv Stuff/Coding/lapsus/downfall/downfall.db', timeout = 1)
 c = con.cursor()
 
 class DBFail:
@@ -20,24 +20,32 @@ class UserTrait:
 
 def new_user(user_id, editor_level, youtube, bio):
     try:
-        c.execute("INSERT INTO users VALUES (?, ?, ?, ?)", (user_id, editor_level, youtube, bio))
+        c.execute("INSERT INTO users VALUES (?, ?, ?, ?)", [int(user_id), editor_level, youtube, bio])
         con.commit()
     except:
         return DBFail("Failed to create user.")
 
 def modify_user(user_id, trait, entry):
     try:
-        c.execute("UPDATE users SET ? = ? WHERE user_id = ?", (trait, entry, user_id))  
+        c.execute("UPDATE users SET ? = ? WHERE user_id = ?", [trait, entry, int(user_id)])  
         con.commit()
     except:
         return DBFail("Failed to modify user.")
 
-def get_user(user_id):
+def get_user(user_id, create_new = True):
     try:
-        c.execute("SELECT * FROM users WHERE user_id = ?", (user_id))
-        return c.fetchone()
+        c.execute("SELECT * FROM users WHERE user_id = ?", [int(user_id)])
+        user = c.fetchone()
     except:
         return DBFail("Failed to get user.")
+    if user is not None or not create_new:
+        return user
+    try:
+        new_user(user_id, 0, None, None)
+    except:
+        return DBFail("Failed to create new user after finding none.")
+    c.execute("SELECT * FROM users WHERE user_id = ?", [int(user_id)])
+    return c.fetchone()
 
 def all_users():
     try:
@@ -48,7 +56,7 @@ def all_users():
 
 def delete_user(user_id):
     try:
-        c.execute("DELETE FROM users WHERE user_id = ?", (user_id))
+        c.execute("DELETE FROM users WHERE user_id = ?", [int(user_id)])
         con.commit()
     except:
         return DBFail("Failed to delete user.")
@@ -64,21 +72,26 @@ class ApplicationTrait:
 
 def new_application(user_id, url, prerecs):
     try:
-        c.execute("INSERT INTO applications (status, user_id, url, prerecs) VALUES (?, ?, ?, ?)", (-1, int(user_id), str(url), int(prerecs)))
+        c.execute("INSERT INTO applications (status, user_id, url, prerecs) VALUES (?, ?, ?, ?)", [-1, int(user_id), str(url), int(prerecs)])
         con.commit()
     except:
         return DBFail("Failed to create application.")
+    try:
+        c.execute("SELECT * FROM applications WHERE rowid = ?", [c.lastrowid])
+        return c.fetchone()[0]
+    except:
+        return DBFail("Failed to get application ID.")
 
 def modify_application(application_id, trait, entry):
     try:
-        c.execute("UPDATE applications SET ? = ? WHERE application_id = ?", (trait, entry, application_id))  
+        c.execute("UPDATE applications SET ? = ? WHERE application_id = ?", [trait, entry, application_id])  
         con.commit()
     except:
         return DBFail("Failed to modify application.")
 
 def get_application(application_id):
     try:
-        c.execute("SELECT * FROM applications WHERE application_id = ?", (application_id))
+        c.execute("SELECT * FROM applications WHERE application_id = ?", [application_id])
         return c.fetchone()
     except:
         return DBFail("Failed to get application.")
@@ -92,7 +105,7 @@ def all_applications():
 
 def delete_application(application_id):
     try:
-        c.execute("DELETE FROM applications WHERE application_id = ?", (application_id))
+        c.execute("DELETE FROM applications WHERE application_id = ?", [application_id])
         con.commit()
     except:
         return DBFail("Failed to delete application.")
